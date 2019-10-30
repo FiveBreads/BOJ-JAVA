9dafe5af039d99b998acc00d23b0f9fbc2d7a32d
FiveBreads <49057625+FiveBreads@users.noreply.github.com>
2019-10-30 20:20:43 +0900

    알고리즘 문제 풀이

---
+++ Num2579.java
@@ -0,0 +1,29 @@
+package dp1;
+
+import java.io.*;
+import java.util.*;
+public class Num2579 {
+	
+	static int[] s;
+	static boolean[] p;
+	static int sum = 0;
+	
+	public static void main(String[] args) throws IOException{
+		
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		//int n = Integer.parseInt(br.readLine());
+		
+		//s = new int[n+1];
+		//p = new boolean[n+1];
+		
+		//for(int i = 1; i <= n; i++) {
+		//	s[i] = Integer.parseInt(br.readLine());
+		//}
+		
+		//for(int i = 1; i <= n; i++) {
+			
+		//}
+		
+	}
+
+}
