bbe7207e3f8fec5fdded1dec85d70eaa080a8adb
FiveBreads <49057625+FiveBreads@users.noreply.github.com>
2019-11-18 22:04:26 +0900

    BOJ-경북대2019-수식트리(런타임에러)

--- BOJ/src/gyeong_in_shake/Num17501.java
+++ BOJ/src/gyeong_in_shake/Num17501.java
@@ -1,39 +1,79 @@
 package gyeong_in_shake;
 import java.util.*;
 import java.io.*;
-public class Num17501 {
+public class Num17501 {	
 	static StringTokenizer st;
+	static String[][] input;
+	static int[] numbers;
+	static int front, back;
+	
+	static int dfs(String root, String left, String right, int leftNum, int rightNum, boolean flag) {
+		int lNum = 0, rNum = 0, max = 0, min = 0;
+		
+		if(left.equals("+") || left.equals("-")) {
+			lNum = Integer.parseInt(input[leftNum][1]);
+			rNum = Integer.parseInt(input[leftNum][2]);
+			max = dfs(left, input[lNum][0], input[rNum][0] ,lNum, rNum, false);
+			flag = true;
+		}
+		flag = false;
+		
+		if(right.equals("+") || right.equals("-")) {
+			lNum = Integer.parseInt(input[rightNum][1]);
+			rNum = Integer.parseInt(input[rightNum][2]);
+			min = dfs(right, input[rNum][0], input[rNum][0] ,lNum, rNum, false);
+			flag = true;
+		}		
+		
+		if(!flag) {
+			if(front == back) 
+				return numbers[front];
+			if(root.equals("+")) {
+				min = numbers[front++]; 
+				max = numbers[front++];
+				return min + max;
+			}else { // root.equals("-")
+				min = numbers[front++]; 
+				max = numbers[back++];
+				return max - min;
+			}
+		}else {
+			if(root.equals("+")) {				
+				return min + max;
+			}else { // root.equals("-")				
+				return max - min;
+			}
+		}				
+	}	
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());		
-		int[] op = new int[N];	
-		int countM = 0;
-		long answer = 0;
-		for(int i = 0; i < N; i++) {			
-			op[i] = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		int opEnd = 2*N - 1;
+		input = new String[opEnd + 1][3];
+		numbers = new int[opEnd + 1];
+		front = 1;
+		back = N;
+		numbers[0] = -10001;
+		for(int i = 1; i <= N; i++) {
+			String a = br.readLine();
+			numbers[i] = Integer.parseInt(a);
+			input[i][0] = a;
 		}
-		Arrays.sort(op);
-		int len = 2*N-1;
-		for(int i = N; i < 2*N-1; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			String a = st.nextToken();
-			if(a.equals("-")) {
-				countM++;
-				System.out.println("마이너스 갯수 : " + countM);
-			}
+		for(int i = N + 1; i <= opEnd; i++) {
+			numbers[i] = 10001;
+			st = new StringTokenizer(br.readLine());
+			input[i][0] = st.nextToken();
+			input[i][1] = st.nextToken();
+			input[i][2] = st.nextToken();
 		}
-		int front = 0;
-		int back = op.length - 1;
-		while(countM > 0) {
-			answer += (op[back] - op[front]);	
-			front++;
-			back--;
-			countM--;
+		Arrays.sort(numbers);
+		for(int str : numbers) {
+			System.out.println(str);
 		}
-		for(int i = front; i <= back; i++) {
-			answer += op[i];
-		}
-		System.out.println(answer);		
+		int leftNum = Integer.parseInt(input[opEnd][1]);
+		int rightNum = Integer.parseInt(input[opEnd][2]);
+		int result = dfs(input[opEnd][0], input[leftNum][0], input[rightNum][0], leftNum, rightNum, false);
+		System.out.println(result);
 	}
 }
