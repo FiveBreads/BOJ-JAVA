fb7fef555264a33a9698e6383b132a022eb1cdb9
FiveBreads <49057625+FiveBreads@users.noreply.github.com>
2019-11-21 20:49:33 +0900

    별찍기의 향연

--- BOJ/src/step1/Num10991.java
+++ BOJ/src/step1/Num10991.java
@@ -1,10 +1,26 @@
 package step1;
-
+import java.util.*;
 public class Num10991 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		//첫번째 별은 n번째 위치에 찍힐것.
+		//마지막 별은 2*n-1번째 위치에 찍힐것.		
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= n - i; j++) {
+				System.out.printf(" ");
+			}
+			for(int j = 1; j <= (i*2-1); j++) {
+				if(j %2 == 0) {
+					System.out.printf(" ");
+				}else {
+					System.out.printf("*");
+				}
+			}
+			System.out.println();
+		}
 	}
-
 }
--- BOJ/src/step1/Num2522.java
+++ BOJ/src/step1/Num2522.java
@@ -1,10 +1,31 @@
 package step1;
-
+import java.util.*;
 public class Num2522 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		
+		for(int i = 1; i <= n; i++) {
+			for(int j = n-i; j > 0; j--) {
+				System.out.print(" ");
+			}
+			for(int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		for(int i = 1; i <= n - 1; i++) {
+			for(int j = 1; j <= i; j++) {
+				System.out.print(" ");
+			}
+			for(int j = n-i; j > 0; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
-
 }
