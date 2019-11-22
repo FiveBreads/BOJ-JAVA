5d34405928c44514efb39974966b20217aa0b20b
FiveBreads <49057625+FiveBreads@users.noreply.github.com>
2019-11-22 15:35:18 +0900

    입출력 점검 마무리(별찍기17)

--- BOJ/src/step1/Num10992.java
+++ BOJ/src/step1/Num10992.java
@@ -1,10 +1,30 @@
 package step1;
-
+import java.util.*;
 public class Num10992 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		for(int i = 1; i <= n; i++) {			
+			if(i == n) {
+				for(int j = 1; j <= 2*n - 1; j++) {
+					System.out.printf("*");
+				}
+			}else {
+				for(int j = 1; j <= n-i; j++) {
+					System.out.printf(" ");
+				}
+				for(int j = 1; j <= (i * 2 - 1); j++) {
+					if(j == 1 || j == (i * 2 - 1)) {
+						System.out.print("*");
+					}else {
+						System.out.printf(" ");
+					}
+				}				
+			}
+			System.out.println();
+		}			
 	}
-
 }
