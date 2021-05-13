5ca7ba5b6139bb72f5b86e89c3e6cb561f8d2bce
Gunwoo Park <prayzz12@gmail.com>
2021-05-13 11:58:17 +0900

    트리 1068 2차 풀이 성공

--- BOJ/src/BOJ/tree/Num1068.java
+++ BOJ/src/BOJ/tree/Num1068.java
@@ -3,80 +3,95 @@ package BOJ.tree;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
 
 public class Num1068 {
-  static TreeNode tree;
+  static TreeNode[] tree;
   static int cnt = 0;
+  static ArrayList<Integer> delList = new ArrayList<>();
 
-  static void checkAndMakeTreeNode(TreeNode node, int parentData, int data){
-    if (node.data == parentData) {
-      if (node.left == null) node.left = new TreeNode(data);
-      else node.right = new TreeNode(data);
+  static void makeDeleteList(int targetIndex){
+    delList.add(targetIndex);
+    ArrayList<Integer> child = tree[targetIndex].getChild();
+    for (int loop = 0; loop < child.size(); loop++) {
+      makeDeleteList(child.get(loop));
     }
-    if (node.left != null) checkAndMakeTreeNode(node.left, parentData, data);
-    if (node.right != null) checkAndMakeTreeNode(node.right, parentData, data);
   }
 
-  static boolean delChildNode(TreeNode node, int data){
-    if (node.data == data) return true;
-    if (node.left != null) {
-      if (delChildNode(node.left, data)){
-        node.left = null;
+  static void deleteNodes(){
+    for (int loop = 0; loop < delList.size(); loop++) {
+      int parentIndex = tree[delList.get(loop)].getParent();
+      if (parentIndex == -1) {
+        tree = null;
+        break;
       }
+      tree[parentIndex].deleteChild(delList.get(loop));
     }
-    if (node.right != null) {
-      if (delChildNode(node.right, data)){
-        node.right = null;
-      }
-    }
-    return false;
   }
 
-  static void getLeafNode(TreeNode node){
-    boolean isLeaf = false;
-    if (node.left != null) {
-      isLeaf = true;
-      getLeafNode(node.left);
-    }
-    if (node.right != null) {
-      isLeaf = true;
-      getLeafNode(node.right);
-    }
-    if (isLeaf){
+  static void getLeafNodeCount(int index){
+    if (tree[index].getChild().size() == 0){
       cnt++;
+      return;
+    }
+    for (int loop = 0; loop < tree[index].getChild().size(); loop++) {
+      getLeafNodeCount(tree[index].getChild().get(loop));
     }
   }
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    int i = 0;
-    while (st.hasMoreTokens()){
-      int parent = Integer.parseInt(st.nextToken());
-      if (parent == -1) {
-        tree = new TreeNode(i);
-      }
-      else {
-        checkAndMakeTreeNode(tree, parent, i);
-      }
-      i++;
+
+    tree = new TreeNode[n];
+
+    String[] parents = br.readLine().split(" ");
+    int del = Integer.parseInt(br.readLine());
+
+    for (int loop = 0; loop < n; loop++) {
+      tree[loop] = new TreeNode();
     }
-    int targetIndex = Integer.parseInt(br.readLine());
-    if(!delChildNode(tree, targetIndex)){
-      getLeafNode(tree);
+
+    int root = 0;
+
+    for (int loop = 0; loop < n; loop++) {
+      int parent = Integer.parseInt(parents[loop]);
+      tree[loop].setParent(parent);
+      if (parent >= 0){
+        tree[parent].setChild(loop);
+      }else {
+        root = loop;
+      }
+    }
+    makeDeleteList(del);
+    deleteNodes();
+    if (tree != null){
+      getLeafNodeCount(root);
     }
     System.out.println(cnt);
   }
 }
 class TreeNode {
-  int data;
-  TreeNode left;
-  TreeNode right;
-  public TreeNode(int data){
-    this.data = data;
-    left = null;
-    right = null;
+  private int parent;
+  private ArrayList<Integer> child = new ArrayList<>();
+
+  public void deleteChild(int target){
+    child.remove(child.indexOf(target));
+  }
+
+  public void setChild(int childNum){
+    child.add(childNum);
+  }
+
+  public ArrayList<Integer> getChild(){
+    return child;
+  }
+
+  public int getParent() {
+    return parent;
+  }
+
+  public void setParent(int parent) {
+    this.parent = parent;
   }
 }
