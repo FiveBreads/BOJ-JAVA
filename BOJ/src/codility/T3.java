0632eabf01187c141515dc48011956ee5420ce11
Gunwoo Park <prayzz12@gmail.com>
2021-06-23 21:04:16 +0900

    복기 최종 완료

--- BOJ/src/codility/T3.java
+++ BOJ/src/codility/T3.java
@@ -7,38 +7,47 @@ import java.util.LinkedList;
 public class T3 {
     public static int solution(int N) {
         N += 1;
-        if (N <= 10) return N;
+        if (N <= 10){
+            return N;
+        }
         String strN = String.valueOf(N);
-        int len = strN.length();
-        int index = 0;
+
         Deque<Integer> dq = new LinkedList<>();
         dq.offer(strN.charAt(0) - '0');
-        for (int i = 1; i < len; i++) {
-           if (strN.charAt(i) == strN.charAt(i -1)){
-               index = i;
-               int tmp = strN.charAt(i) - '0';
-               tmp += 1;
-               while (tmp == 10){
-                   if (index < 0){
-                       dq.offer(1);
-                       index = 0;
-                       len += 1;
-                       break;
-                   }
-                   tmp = dq.removeLast();
-                   tmp += 1;
-                   index -= 1;
-               }
-               tmp = 0;
-               for (int j = index + 1; j < len; j++) {
-                   dq.offer(tmp);
-                   if (tmp == 0) tmp++;
-                   else tmp--;
-               }
-               break;
-           }else {
-               dq.offer(strN.charAt(i) - '0');
-           }
+
+        int index = 0;
+        for (int i = 1; i < strN.length(); i++) {
+            if (strN.charAt(i) == strN.charAt(i - 1)){
+                int plus = strN.charAt(i) - '0';
+                plus += 1;
+                index = i;
+                boolean isShift = false;
+                while (plus == 10){
+                    if (dq.isEmpty()) {
+                        dq.offer(1);
+                        index = -1;
+                        isShift = true;
+                        break;
+                    }
+                    int left = dq.removeLast();
+                    plus = left + 1;
+
+                    if (!dq.isEmpty() && dq.getLast() == plus){
+                        plus++;
+                    }
+                    index--;
+                }
+                if (!isShift) dq.offer(plus);
+                int tmp = 0;
+                for (int j = index + 1; j < strN.length(); j++) {
+                    dq.offer(tmp);
+                    if (tmp == 0) tmp++;
+                    else tmp--;
+                }
+                break;
+            }else{
+                dq.offer(strN.charAt(i) - '0');
+            }
         }
         StringBuilder result = new StringBuilder();
         while (!dq.isEmpty()){
